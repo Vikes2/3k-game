ebc2482325a8b0c040f2f0e90011f2a24c7b4f79
Vikes2 <muczynski.pawel@gmail.com>
2019-01-07 11:29:20 +0100

    Added observer design pattern

--- threeK_game/consumers.py
+++ threeK_game/consumers.py
@@ -1,26 +1,51 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .game import *
 import json
 
 queue = []
 
 class GameConsumer(WebsocketConsumer):
-    def connect(self):
-        self.game_name = self.scope['user']
-        self.game_group_name = 'game_1'
 
-        print(self.game_name)
 
+
+    def update(self, arg):
+        # tu trza connect to grupa konkretna z param from Match in Game
+        self._match_start = arg
+        self.game_group_name = 'game_' + str(arg)
         async_to_sync(self.channel_layer.group_add)(
             self.game_group_name,
             self.channel_name
         )
+        self.is_in_group = True
+        #self.group_message("elo w grupie")
 
+
+    def connect(self):
+        self.is_in_group = False
+        self._match = None
+        self._match_start = False
         self.accept()
 
         self.send(text_data=json.dumps({
             'message': "polaczono ;)"
         }))
+
+        self.game_manager = GameManager()
+        user = self.scope['user']
+        self.game_manager.connect_player(self)
+        print(self.game_manager.queue.print_content())
+        # self.game_name = self.scope['user']
+        # self.game_group_name = 'game_1'
+
+        # print(self.game_name)
+
+        # async_to_sync(self.channel_layer.group_add)(
+        #     self.game_group_name,
+        #     self.channel_name
+        # )
+
+
 
     def disconnect(self, close_code):
         pass
@@ -29,13 +54,20 @@ class GameConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.game_group_name,
-            {
-                'type': 'game_message',
-                'message': message
-            }
-        )
+
+    def group_message(self, text):
+
+        if self.is_in_group == False:
+            print("Nie jestes w grupie")
+        else:
+            async_to_sync(self.channel_layer.group_send)(
+                self.game_group_name,
+                {
+                    'type': 'game_message',
+                    'message': text
+                }
+            )
+
     def game_message(self, event):
         message = event['message']
 
