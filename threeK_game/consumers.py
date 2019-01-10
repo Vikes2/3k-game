5147177791573efea76787d7e9ea7fddd19b191c
Wojciech Haniewski <w.haniewski@gmail.com>
2019-01-10 11:17:35 +0100

    Layout

--- threeK_game/consumers.py
+++ threeK_game/consumers.py
@@ -1,25 +1,37 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .game import *
 import json
 
 queue = []
 
 class GameConsumer(WebsocketConsumer):
-    def connect(self):
-        self.game_name = self.scope['user']
-        self.game_group_name = 'game_1'
 
-        print(self.game_name)
+    def update(self, arg):
+        # tu trza connect to grupa konkretna z param from Match in Game
+        self._match_start = arg
+        self.game_group_name = 'game_' + str(arg)
 
         async_to_sync(self.channel_layer.group_add)(
             self.game_group_name,
             self.channel_name
         )
+        self.is_in_group = True
 
+    def connect(self):
+        self.is_in_group = False
+        self._match = None
+        self._match_start = False
         self.accept()
+        
+        self.game_manager = GameManager()
+        user = self.scope['user']
+        self.game_manager.connect_player(self)
+        print(self.game_manager.queue.print_content())
 
         self.send(text_data=json.dumps({
-            'message': "polaczono ;)"
+            'type': 'connect',
+            'message': "connected successfully"
         }))
 
     def disconnect(self, close_code):
@@ -36,6 +48,36 @@ class GameConsumer(WebsocketConsumer):
                 'message': message
             }
         )
+
+    def group_message(self, text):
+
+        if self.is_in_group == False:
+            print("Nie jestes w grupie")
+        else:
+            async_to_sync(self.channel_layer.group_send)(
+                self.game_group_name,
+                {
+                    'type': 'game_message',
+                    'message': text
+                }
+            )
+    def notify_about_start(self, match_id):
+        async_to_sync(self.channel_layer.group_send)(
+            self.game_group_name,
+            {
+                'type': 'create_match_message',
+                'message': match_id
+            }
+        )
+
+    def create_match_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'type': 'created_game',
+            'match_id': message
+        }))
+
     def game_message(self, event):
         message = event['message']
 
---
+++ threeK_game/game.py
@@ -0,0 +1,98 @@
+import abc
+from .models import Match as mMatch, Profile
+
+class QueueI(abc.ABC):
+    @abc.abstractmethod
+    def len(self):
+        pass
+    @abc.abstractmethod
+    def pop(self):
+        pass
+    @abc.abstractmethod
+    def push(self, value):
+        pass
+    @abc.abstractmethod
+    def is_empty(self):
+        pass
+
+class Queue(QueueI):
+    def __init__(self, container):
+        self.players = container
+
+    def len(self):
+        return len(self.players)
+
+    def pop(self):
+        return self.players.pop(0) if self.len() > 0 else None
+
+    def push(self, value):
+        self.players.append(value)
+
+    def is_empty(self):
+        return (self.len() == 0)
+
+    def print_content(self):
+        # return tuple(self.players)
+        return self.players
+
+class GameManager(object):
+    __instance = None
+    def __new__(cls):
+        if GameManager.__instance is None:
+            GameManager.__instance = object.__new__(cls)
+            cls.queue = Queue([])
+            cls.active_matches = []    
+        return GameManager.__instance
+
+    def connect_player(self, consumer):
+        #user = consumer.scope['user'].username
+        self.queue.push(consumer)
+        print(self.queue.len(), end="============================ len queue\n")
+        if self.queue.len() > 1:
+            self.create_new_game()
+    
+    def create_new_game(self):
+        player_a = self.queue.pop()
+        player_b = self.queue.pop()
+        new_match= Match(player_a.scope['user'].username, player_b.scope['user'].username)
+        print("match created")
+        new_match.attach(player_a)
+        new_match.attach(player_b)
+        new_match.match_start = new_match.match_id
+        print(new_match.match_id)
+        self.active_matches.append(new_match)
+
+class Match:
+    def __init__(self, username_a, username_b):
+        self._observers = set()
+        self.create_match_model(username_a, username_b)
+
+    def create_match_model(self, username_a, username_b):
+        print(username_a, username_b, end="---")
+        user_a = Profile.objects.get(user__username=username_a)
+        user_b = Profile.objects.get(user__username=username_b)
+        self.match_model = mMatch.objects.create(playerA=user_a, playerB=user_b)
+        self.match_id = self.match_model.id
+
+    def attach(self, observer):
+        observer._match = self
+        self._observers.add(observer)
+
+    def detach(self, observer):
+        observer._match = None
+        self._observers.discard(observer)
+
+    def _notify(self):
+        for observer in self._observers:
+            observer.update(self._match_start)
+        self._observers.pop().notify_about_start(self.match_id)
+        
+
+    @property
+    def match_start(self):
+        return self._match_start
+
+    @match_start.setter
+    def match_start(self, arg):
+        self._match_start = arg
+        self._notify()
