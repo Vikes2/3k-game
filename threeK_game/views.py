675b995b6f6eb815e6b456ed52d89da2d52b1d65
Marcin Pawelski <marcinpawelski.96@gmail.com>
2019-01-14 10:47:59 +0100

    Checking islogged,  updated UI for game window

--- threeK_game/views.py
+++ threeK_game/views.py
@@ -1,12 +1,18 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, "threeK_game/index.html")
+    if request.user.is_authenticated:
+        return redirect('/home')
+    else:
+        return render(request, "threeK_game/index.html")
 
 def home(request):
-    return render(request, "threeK_game/home.html")
+    if not request.user.is_authenticated:
+        return redirect('/')
+    else:
+        return render(request, "threeK_game/home.html")
 
 def signup(request):
     if request.method == 'POST':
@@ -21,6 +27,7 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 
 def game(request):
     return render(request, 'threeK_game/game.html')
